c64267c2d209227407d081c9e7231d9120651c1b
vab9 <vbrinkhege@uos.de>
2016-08-11 17:51:54 +0200

    travising... building

--- ci/install-glslangValidator.sh
+++ ci/install-glslangValidator.sh
@@ -2,10 +2,23 @@
 
 set -e
 
+# echo "need to install glslangValidator..."
+# mkdir validator/
+# cd validator/
+# wget --no-check-certificate "https://cvs.khronos.org/svn/repos/ogl/trunk/ecosystem/public/sdk/tools/glslang/Install/Linux/glslangValidator"
+# chmod +x glslangValidator
+# cd ../
+# echo "installed glslangValidator to $PWD/validator/glslangValidator"
+
 echo "need to install glslangValidator..."
 mkdir validator/
 cd validator/
-wget --no-check-certificate "https://cvs.khronos.org/svn/repos/ogl/trunk/ecosystem/public/sdk/tools/glslang/Install/Linux/glslangValidator"
-chmod +x glslangValidator
+git clone git@github.com:KhronosGroup/glslang.git
+cd glslang
+git clone https://github.com/google/googletest.git External/googletest
 cd ../
-echo "installed glslangValidator to $PWD/validator/glslangValidator"
+cmake -GNinja -DCMAKE_BUILD_TYPE={Debug|Release|RelWithDebInfo} \
+      -DCMAKE_INSTALL_PREFIX=`pwd`/install glslang/
+ninja install
+cd ../
+echo "installed glslangValidator to /validator/glslangValidator"
